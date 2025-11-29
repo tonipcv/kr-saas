5c7a8d75d45d239f488cbc22f9c1b0cb74988d4a
tonipcv <xppsalvador@gmail.com>
2025-11-28 23:54:14 -0300

    risk 5 - deitado com mi

--- trigger/check-stuck-deliveries.ts
+++ trigger/check-stuck-deliveries.ts
@@ -1,4 +1,4 @@
-import { schedules, tasks } from "@trigger.dev/sdk/v3";
+import { schedules, tasks } from "@trigger.dev/sdk";
 import { PrismaClient } from "@prisma/client";
 import type { deliverWebhook } from "./deliver-webhook";
 
--- trigger/deliver-webhook.ts
+++ trigger/deliver-webhook.ts
@@ -1,4 +1,4 @@
-import { task } from "@trigger.dev/sdk/v3";
+import { task } from "@trigger.dev/sdk";
 import { PrismaClient } from "@prisma/client";
 import { createHmac } from "crypto";
 
